dc067e4359bda4452c8586d42467c65dc62cd7f0
Rohit Dhas <rohitdhas666@gmail.com>
2022-11-06 21:42:29 +0530

    added additional config

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,5 +4,13 @@ module.exports = {
   // https://storybook.js.org/docs/react/configure/typescript#mainjs-configuration
   typescript: {
     check: true, // type-check stories during Storybook build
-  }
+  },
+  webpackFinal: async config => {
+    config.module.rules.push({
+      test: /\.mjs$/,
+      include: /node_modules/,
+      type: 'javascript/auto',
+    });
+    return config;
+  },
 };
