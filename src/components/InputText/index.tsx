66be76d69096a4496a8a4c240fd4dac78509f7aa
Rohit Dhas <rohitdhas666@gmail.com>
2022-11-07 22:22:54 +0530

    built input text component

---
+++ src/components/InputText/index.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { InputTextTypes } from './InputText.types';
+
+const Button: React.FC<InputTextTypes> = props => {
+  const { searchIcon, ...rest } = props;
+  return (
+    <form className="group relative">
+      {searchIcon ? (
+        <svg
+          width="20"
+          height="20"
+          fill="currentColor"
+          className="absolute left-3 top-1/2 -mt-2.5 text-slate-400 pointer-events-none group-focus-within:text-blue-500"
+          aria-hidden="true"
+        >
+          <path
+            fill-rule="evenodd"
+            clip-rule="evenodd"
+            d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+          />
+        </svg>
+      ) : (
+        <></>
+      )}
+      <input
+        className={`focus:ring-2 focus:ring-blue-500 focus:outline-none appearance-none w-full text-sm leading-6 text-slate-900 placeholder-slate-400 rounded-md py-2 ring-1 ring-slate-200 shadow-sm ${
+          searchIcon ? 'pl-10' : 'px-2'
+        }`}
+        type="text"
+        aria-label="input-text"
+        {...rest}
+      />
+    </form>
+  );
+};
+
+export default Button;
