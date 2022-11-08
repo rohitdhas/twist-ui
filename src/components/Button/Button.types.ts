fed444e80dbb4e9e37cc06f31328e3aa193fab6f
Rohit Dhas <rohitdhas666@gmail.com>
2022-11-08 21:50:32 +0530

    added icon to button component

--- src/components/Button/Button.types.ts
+++ src/components/Button/Button.types.ts
@@ -1,9 +1,11 @@
 import { ButtonHTMLAttributes } from 'react';
+import { IconProp } from '@fortawesome/fontawesome-svg-core';
 
 type ButtonVariants = 'primary' | 'secondary';
 
 export interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
   label?: string;
+  icon?: IconProp;
   varient?: ButtonVariants;
 }
 
--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { ButtonProps } from './Button.types';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const Button: React.FC<ButtonProps> = React.memo(
   React.forwardRef((props, ref) => {
-    const { label, ...restProps } = props;
+    const { label, icon, ...restProps } = props;
     const elementRef = React.useRef(null);
 
     React.useImperativeHandle(ref, () => ({
@@ -17,6 +18,7 @@ const Button: React.FC<ButtonProps> = React.memo(
         className="font-bold text-white px-4 py-2 rounded-md bg-purple-500 hover:bg-purple-700 transition-all"
         {...restProps}
       >
+        {icon ? <FontAwesomeIcon className="mr-2" icon={icon} /> : <></>}
         {label || 'Click Me!'}
       </button>
     );
