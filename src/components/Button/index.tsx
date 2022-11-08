afe5f475c63987004b1559ab63687ac780d2519e
Rohit Dhas <rohitdhas666@gmail.com>
2022-11-08 21:23:04 +0530

    updated button component

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,16 +1,26 @@
 import React from 'react';
 import { ButtonProps } from './Button.types';
 
-const Button: React.FC<ButtonProps> = props => {
-  const { label, ...rest } = props;
-  return (
-    <button
-      className="font-bold text-white px-4 py-2 rounded-md bg-purple-500 hover:bg-purple-700 transition-all"
-      {...rest}
-    >
-      {label || 'Click Me!'}
-    </button>
-  );
-};
+const Button: React.FC<ButtonProps> = React.memo(
+  React.forwardRef((props, ref) => {
+    const { label, ...restProps } = props;
+    const elementRef = React.useRef(null);
+
+    React.useImperativeHandle(ref, () => ({
+      props,
+      getElement: () => elementRef.current,
+    }));
+
+    return (
+      <button
+        ref={elementRef}
+        className="font-bold text-white px-4 py-2 rounded-md bg-purple-500 hover:bg-purple-700 transition-all"
+        {...restProps}
+      >
+        {label || 'Click Me!'}
+      </button>
+    );
+  })
+);
 
 export default Button;
