6f1a6de2fea2ce053678513e27d9ccc2f6ae628f
Rohit Dhas <rohitdhas666@gmail.com>
2022-11-13 13:57:19 +0530

    added new color scheme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,22 @@
+// const { colors: defaultColors } = require('tailwindcss/defaultTheme');
+
+// const colors = {
+//   ...defaultColors,
+//   ...{
+//     primary: '#23A6F0',
+//     'primary-dark': '#2A7CC7',
+//     'primary-disabled': '#8EC2F2',
+//     success: '#2DC071',
+//     'success-dark': '#1EB062',
+//     alert: '#E77C40',
+//     'alert-dark': '#CA652C',
+//     danger: '#E74040',
+//     'danger-dark': '#BB2C2C',
+//     muted: '#BDBDBD',
+//     'text-color': '#252B42',
+//   },
+// };
+
 module.exports = {
   purge: {
     content: ['./src/components/**/*.tsx', './src/components/**/*.styled.tsx'],
@@ -5,7 +24,21 @@ module.exports = {
   },
   darkMode: false,
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#23A6F0',
+        'primary-dark': '#2A7CC7',
+        'primary-disabled': '#8EC2F2',
+        success: '#2DC071',
+        'success-dark': '#1EB062',
+        alert: '#E77C40',
+        'alert-dark': '#CA652C',
+        danger: '#E74040',
+        'danger-dark': '#BB2C2C',
+        muted: '#BDBDBD',
+        'text-color': '#252B42',
+      },
+    },
   },
   variants: {
     extend: {},
