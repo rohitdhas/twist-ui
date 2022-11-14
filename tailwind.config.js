dfda5543b93c5e44030640c51addea0f9ee6e0f9
Rohit Dhas <rohitdhas666@gmail.com>
2022-11-14 19:47:13 +0530

    removed unused styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,6 @@
-// const { colors: defaultColors } = require('tailwindcss/defaultTheme');
-
-// const colors = {
-//   ...defaultColors,
-//   ...{
-//     primary: '#23A6F0',
-//     'primary-dark': '#2A7CC7',
-//     'primary-disabled': '#8EC2F2',
-//     success: '#2DC071',
-//     'success-dark': '#1EB062',
-//     alert: '#E77C40',
-//     'alert-dark': '#CA652C',
-//     danger: '#E74040',
-//     'danger-dark': '#BB2C2C',
-//     muted: '#BDBDBD',
-//     'text-color': '#252B42',
-//   },
-// };
-
 module.exports = {
   purge: {
-    content: ['./src/components/**/*.tsx', './src/components/**/*.styled.tsx'],
+    content: ['./src/components/**/*.tsx'],
     enabled: process.env.NODE_ENV === 'publish',
   },
   darkMode: false,
