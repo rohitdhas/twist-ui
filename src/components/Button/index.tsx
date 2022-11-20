fd69d533a3decbc5880e773e56489b964636e464
Rohit Dhas <rohitdhas666@gmail.com>
2022-11-20 14:34:57 +0530

    added focus ring to button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -8,24 +8,28 @@ const buttonStyles = {
     hover: 'hover:bg-primary-dark',
     outline: 'border-primary',
     text: 'text-primary',
+    ring: 'focus:outline-none focus:ring focus:ring-blue-300',
   },
   success: {
     bg: 'bg-success',
     hover: 'hover:bg-success-dark',
     outline: 'border-success',
     text: 'text-success',
+    ring: 'focus:outline-none focus:ring focus:ring-green-300',
   },
   danger: {
     bg: 'bg-danger',
     hover: 'hover:bg-danger-dark',
     outline: 'border-danger',
     text: 'text-danger',
+    ring: 'focus:outline-none focus:ring focus:ring-red-300',
   },
   warn: {
     bg: 'bg-alert',
     hover: 'hover:bg-alert-dark',
     outline: 'border-alert',
     text: 'text-alert',
+    ring: 'focus:outline-none focus:ring focus:ring-yellow-500',
   },
 };
 
@@ -50,7 +54,7 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
       return (
         <button
           ref={ref}
-          className={`font-bold flex align items-center justify-center ${varientClasses.text} px-4 py-2 rounded-md border-2 ${varientClasses.outline} ${varientClasses.hover} transition-all hover:text-white`}
+          className={`font-bold flex align items-center justify-center ${varientClasses.text} px-4 py-2 rounded-md border-2 ${varientClasses.outline} ${varientClasses.hover} ${varientClasses.ring} transition-all hover:text-white`}
           {...restProps}
         >
           {children || label || 'Click Me!'}
@@ -61,7 +65,7 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
     return (
       <button
         ref={ref}
-        className={`font-bold flex align items-center justify-center text-white px-4 py-2 rounded-md ${varientClasses.bg} ${varientClasses.hover} transition-all`}
+        className={`font-bold flex align items-center justify-center text-white px-4 py-2 rounded-md ${varientClasses.bg} ${varientClasses.hover} ${varientClasses.ring} transition-all`}
         {...restProps}
       >
         {children || label || 'Click Me!'}
