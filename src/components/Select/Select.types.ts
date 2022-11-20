c6ffcfb8d8310f118780707eef82c885a3b42e5e
Rohit Dhas <rohitdhas666@gmail.com>
2022-11-20 14:36:57 +0530

    component export

--- src/components/Select/Select.types.ts
+++
@@ -1,7 +0,0 @@
-import { SelectHTMLAttributes } from 'react';
-
-export interface Props extends SelectHTMLAttributes<HTMLSelectElement> {
-  itemList: string[];
-}
-
-export type SelectProps = Props;
--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,4 +1,6 @@
 export { default as Button } from './Button';
 export { default as InputText } from './InputText';
 export { default as Textarea } from './Textarea';
-export { default as Select } from './Select';
+export { default as Dropdown } from './DropDown';
+export { default as Radio } from './Radio';
+export { default as Checkbox } from './Checkbox';
